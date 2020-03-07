f7407eb4f0ba5eb0355b62165926c92963be8913
GreyWolfkin <57689976+GreyWolfkin@users.noreply.github.com>
2020-03-06 19:44:36 -0800

    Added SliderPageTest and SliderPageObject classes

---
+++ src/test/java/qtx/SliderPageObject.java
@@ -0,0 +1,48 @@
+package qtx;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class SliderPageObject extends PageObjectSuperClass {
+
+	@FindBy(tagName="input")
+	WebElement slider;
+
+	@FindBy(id="range")
+	WebElement range;
+
+	Actions builder;
+
+	public SliderPageObject(WebDriver driverInstance) {
+		super(driverInstance);
+		builder = new Actions(driverInstance);
+	}
+
+	public SliderPageObject goToPage() {
+		navigate("/horizontal_slider");
+		return this;
+	}
+
+	public SliderPageObject moveSlider() {
+		int widthOfElement = slider.getSize().getWidth();
+		int xOffset = (int)-0.5 * widthOfElement;
+		
+		Action clickAndDragSlider = builder
+				.moveToElement(slider, xOffset, 0)
+				.clickAndHold()
+				.moveByOffset(widthOfElement, 0)
+				.release()
+				.build();
+		
+		clickAndDragSlider.perform();
+		return this;
+	}
+
+	public float confirmResultValue() {
+		return Float.parseFloat(range.getText());
+	}
+
+}
---
+++ src/test/java/qtx/SliderPageTest.java
@@ -0,0 +1,20 @@
+package qtx;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SliderPageTest extends TestSuperClass {
+	
+	@Test
+	public void canAdjustSlider() {
+
+		float actualSliderResult = new SliderPageObject(driver)
+				.goToPage()
+				.moveSlider()
+				.confirmResultValue();
+				
+		float expectedResult = 5;
+		
+		Assert.assertEquals(actualSliderResult, expectedResult, "Slider not succesfully moved");
+	}
+}
